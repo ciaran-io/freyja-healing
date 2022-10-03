d2689dda184343e329358567defe8fa8af2fd6c4
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2022-10-03 03:20:00 +0100

    feat: add header component
    1. Site wide navigation

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,7 +1,108 @@
-<script setup></script>
+<script setup>
+  const store = useStore().value
+  const { treatmentUrl, treatments } = store
+  const showMenu = ref(false)
+  const route = useRoute()
+
+  // close menu on query change
+  watch(
+    () => route.query,
+    () => {
+      showMenu.value = false
+    }
+  )
+</script>
 
 <template>
-  <div>Component: TheHeader</div>
+  <header>
+    <div
+      class="container relative mx-auto mt-8"
+      :class="{ 'flex justify-between': $route.path !== '/' }"
+    >
+      <NuxtLink
+        v-if="$route.path !== '/'"
+        to="/"
+      >
+        <h1 class="text-3xl">
+          Fréyja healing
+          <Icon
+            aria-hidden="true"
+            class="h-6 w-6"
+            name="ic:outline-spa"
+          />
+        </h1>
+      </NuxtLink>
+
+      <button
+        aria-label="open navigation menu"
+        class="float-right"
+        @click="showMenu = !showMenu"
+      >
+        <svg
+          aria-hidden="true"
+          width="40"
+          height="40"
+          viewBox="0 0 40 40"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          class="menu-icon"
+          :class="{ 'menu-icon-animate': showMenu }"
+        >
+          <g
+            id="nav-button"
+            stroke="#7D7777"
+            stroke-width="3.5"
+            stroke-linecap="round"
+          >
+            <path
+              id="line1"
+              d="M2 2H30.8441"
+            />
+            <path
+              id="line2"
+              d="M8.15594 17H37"
+            />
+            <path
+              id="line3"
+              d="M2 32H30.8441"
+            />
+          </g>
+        </svg>
+      </button>
+
+      <NavGlobalMenu
+        :show-menu="showMenu"
+        :treatments="treatments"
+        :treatment-url="treatmentUrl"
+      />
+    </div>
+  </header>
 </template>
 
-<style scoped></style>
+<style scoped lang="postcss">
+  .menu-icon {
+    & #line1,
+    #line3 {
+      transition: all ease-in-out 0.2s;
+    }
+
+    &-animate {
+      & #line1,
+      #line3 {
+        transform-origin: left center;
+      }
+
+      & #line1 {
+        translate: -10% -4%;
+        @apply rotate-45;
+      }
+
+      & #line2 {
+        @apply hidden;
+      }
+      & #line3 {
+        @apply -rotate-45;
+      }
+    }
+  }
+</style>
