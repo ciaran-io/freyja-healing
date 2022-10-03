5c2150df6f9cd3e69df3b34adb1a978a278f4af3
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2022-10-03 03:29:04 +0100

    feat: add social icons component

---
+++ src/components/TheSocialIconBlock.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <nuxt-link
+      aria-label="link to facebook"
+      to="https://www.facebook.com/"
+      target="_blank"
+    >
+      <Icon
+        class="h-8 w-8"
+        name="entypo-social:facebook"
+    /></nuxt-link>
+    <nuxt-link
+      aria-label="link to instagram"
+      to="https://www.instagram.com/"
+      target="_blank"
+    >
+      <Icon
+        class="h-9 w-9"
+        name="ph:instagram-logo-fill"
+    /></nuxt-link>
+
+    <nuxt-link
+      aria-label="link to instagram"
+      to="https://www.twitter.com/"
+      target="_blank"
+    >
+      <Icon
+        class="h-9 w-9"
+        name="entypo-social:twitter"
+    /></nuxt-link>
+  </div>
+</template>
+
+<style lang="postcss" scoped>
+  svg {
+    @apply text-theme-slate;
+  }
+</style>
