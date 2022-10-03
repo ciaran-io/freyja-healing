885d1b5ffe620204f4200288661c9e2564aa46a0
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2022-10-03 02:53:24 +0100

    feat: add base styles and store data
    1. Update global css.
    2. Extend tailwind classes.
    3. Update json data.
    3. Add team images & meta icons.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,5 +5,44 @@ module.exports = {
     './src/layouts/**/*.vue',
     './src/pages/**/*.vue',
   ],
-  theme: {},
+  theme: {
+    container: {
+      padding: '0.5rem',
+    },
+    extend: {
+      backgroundColor: {
+        'theme-base': 'var(--theme-base)',
+        'theme-base-100': 'var(--theme-base-100)',
+        'theme-base-200': 'var(--theme-base-200)',
+
+        'theme-gray': 'var(--theme-gray)',
+        'theme-gray-100': 'var(--theme-gray-100)',
+
+        'theme-slate': 'var(--theme-slate)',
+        'theme-slate-100': 'var(--theme-slate-100)',
+      },
+      colors: {
+        'theme-base': 'var(--theme-base)',
+        'theme-base-100': 'var(--theme-base-100)',
+        'theme-base-200': 'var(--theme-base-200)',
+
+        'theme-gray': 'var(--theme-gray)',
+        'theme-gray-100': 'var(--theme-gray-100)',
+
+        'theme-slate': 'var(--theme-slate)',
+        'theme-slate-100': 'var(--theme-slate-100)',
+      },
+      spacing: {
+        gutter: 'var(--gutter)',
+      },
+      borderRadius: {
+        base: 'var(--rounded-base)',
+      },
+      boxShadow: {
+        primary: 'var(--shadow-primary)',
+        'base-1': 'var(--shadow-shadow-base-1)',
+        'base-2': 'var(--shadow-shadow-base-2)',
+      },
+    },
+  },
 }
