608e41ead84b3f36d96b0c8fdc71b58441e87206
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2022-10-03 03:33:09 +0100

    feat: add footer contact component
    1. Toggle menus in footer on mobile.

---
+++ src/components/FooterContact.vue
@@ -0,0 +1,48 @@
+<script setup>
+  import { showHiddenContent } from '~/utils'
+
+  defineProps({
+    contactInformation: {
+      type: Object,
+      required: true,
+    },
+  })
+
+  function toggleMenu(event) {
+    showHiddenContent(event)
+  }
+</script>
+
+<template>
+  <div class="md:row-span-2 md:justify-self-center">
+    <FooterHeading
+      heading="contact us"
+      @toggle-menu="toggleMenu"
+    />
+
+    <div class="hide-on-mobile">
+      <template
+        v-for="(information, value) in contactInformation"
+        :key="information"
+      >
+        <div class="mt-3">
+          <div v-if="value !== 'opening hours'">
+            <h4>{{ value }}</h4>
+            <p>{{ information }}</p>
+          </div>
+
+          <div v-else>
+            <h4>{{ value }}</h4>
+            <p>{{ information.day }} {{ information.hours }}</p>
+          </div>
+        </div>
+      </template>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="postcss">
+  h4 {
+    @apply font-medium capitalize;
+  }
+</style>
---
+++ src/components/FooterHeading.vue
@@ -0,0 +1,27 @@
+<script setup>
+  defineEmits(['toggleMenu'])
+  defineProps({
+    heading: {
+      type: String,
+      required: true,
+    },
+  })
+</script>
+
+<template>
+  <button
+    class="md:hidden text-theme-slate w-full pb-2 text-xl text-left capitalize"
+    aria-expanded="false"
+    @click="$emit('toggleMenu', $event.target)"
+  >
+    {{ heading }}
+    <Icon
+      class="pointer-events-none float-right h-5 w-5 transition-transform duration-150 md:!hidden"
+      aria-hidden="true"
+      name="gridicons:chevron-down"
+    />
+  </button>
+  <h2 class="md:block hidden pb-3 text-xl capitalize">
+    {{ heading }}
+  </h2>
+</template>
