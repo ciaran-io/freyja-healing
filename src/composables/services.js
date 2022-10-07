503e6aff1927794d5d802cfe5adf40ffd8c6bb21
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2022-10-07 17:59:22 +0100

    refactor: add comments
    1. Provide information what functions are returning.

--- src/composables/services.js
+++ src/composables/services.js
@@ -2,15 +2,16 @@ import data from '~/assets/data/store.json'
 
 const { conditions, reviews, treatments, contactInformation, teamMembers } =
   data
+// Return treatment names in lowercase
 const treatmentNames = computed(
   () => new Set(treatments.map((treatment) => treatment.name.toLowerCase()))
 )
-
+// Replace empty space for hyphen in treatment names
 const treatmentUrl = computed(
   () =>
     new Set([...treatmentNames.value].map((name) => name.replace(/\s/g, '-')))
 )
-
+// Export variables to be globally available in & reactive
 export const useStore = () =>
   useState('store', () => {
     return {
