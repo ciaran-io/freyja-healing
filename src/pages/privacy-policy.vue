67d292810fa2fdf860d317b57fbd99388f30a31d
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2022-10-08 19:10:35 +0100

    fix:(mailto) bad values validator w3.org
    1. Remove empty spaces from body of mailto and replace with correct
    UTF-8 character.
    2. Break mailto information into separate variables for clarity.
    3. Add styles to highlight mail link to users.

--- src/pages/privacy-policy.vue
+++ src/pages/privacy-policy.vue
@@ -8,29 +8,35 @@
       },
     ],
   })
+
+  const emailAddress = 'info@freyja-healing.email'
+  const emailSubject = 'Privacy'
+  const emailBody = 'I would like to request a copy of my data.'
+  const mailtoBody = emailBody.replaceAll(' ', '%20')
+
+  const mailto = `mailto:${emailAddress}?&subject=${emailSubject}&body=${mailtoBody}`
 </script>
 
 <template>
-  <div
-    class="information-card container mx-auto mt-24 mb-16 space-y-6 lg:max-w-4xl"
-  >
-    <h1 class="text-3xl">Privacy policy</h1>
-    <main>
-      <p>
-        Privacy Notice: Fréya healing respects your privacy and is committed to
-        protecting your personal information. We do not sell user information to
-        third parties. Information collected on our website is strictly used to
-        keep reference to customer bookings. We keep records for up to two
-        years. If you have made a booking with us and would like a copy of your
-        data or would like to request deletion of your data you can contact us
-        at
-        <nuxt-link
-          to="mailto:info@freyja-healing.email? &subject=Privacy
-        &body=I would like to request a copy of my data"
-          class="font-semibold"
-          >info@freyja-healing.email
-        </nuxt-link>
-      </p>
-    </main>
+  <div class="container mx-auto mt-24 mb-16 space-y-6 lg:max-w-4xl">
+    <div class="information-card">
+      <h1 class="text-3xl">Privacy policy</h1>
+      <main>
+        <p>
+          Privacy Notice: Fréya healing respects your privacy and is committed
+          to protecting your personal information. We do not sell user
+          information to third parties. Information collected on our website is
+          strictly used to keep reference to customer bookings. We keep records
+          for up to two years. If you have made a booking with us and would like
+          a copy of your data or would like to request deletion of your data you
+          can contact us at
+          <nuxt-link
+            class="font-medium hover:underline"
+            :to="mailto"
+            >info@freyja-healing.email
+          </nuxt-link>
+        </p>
+      </main>
+    </div>
   </div>
 </template>
